e5deb8e661fc14940126d37c0f40714e6fdb80a7
T3AD20 <T3AD20@T3AD20C>
2017-01-06 15:24:58 +0900

    solved

--- src/prob4/Prob4.java
+++ src/prob4/Prob4.java
@@ -14,9 +14,18 @@ public class Prob4 {
 		scanner.close();
 	}
 	
-	public static char[] reverse(String  str) {
+	public static char[] reverse(String str) {
+		char[] c = str.toCharArray();
+		for( int i = 0; i < c.length / 2; i++){
+			char temp = c[ i ]; 
+			c[ i ] = c[ c.length - (i + 1 )  ];
+			c[ c.length - (i + 1) ] = temp;
+		}
+		
+		return c;
 	}
 
 	public static void printCharArray(char[] array){
+		System.out.println( array );
 	}
 }
--- src/prob5/Prob5.java
+++ src/prob5/Prob5.java
@@ -1,8 +1,58 @@
 package prob5;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Prob5 {
 
 	public static void main(String[] args) {
 
+		// 키보드 연결
+		Scanner scanner = new Scanner(System.in);
+
+		// 게임 시작
+		while (true) {
+			
+			// 수 결정하기
+			int min = 1;
+			int max = 100;
+			Random random = new Random();
+			int correctNumber = random.nextInt( max ) + min;
+
+			// 맞추기 시작
+			int tryCount = 1;
+			while (true) {
+				System.out.println(min + "-" + max);
+				System.out.print(tryCount + ">>");
+
+				int guessNumber = scanner.nextInt();
+
+				// 맞췄음
+				if (guessNumber == correctNumber) {
+					System.out.println("맞았습니다.");
+					break;
+				}
+
+				// 틀렸음
+				tryCount++; // 횟 수 증가
+				if (guessNumber > correctNumber) {
+					System.out.println("더 낮게");
+					max = guessNumber; // 추측 범위 조정
+				} else {
+					System.out.println("더 높게");
+					min = guessNumber; // 추측 범위 조정
+				}
+			}
+
+			// 한번 더?
+			System.out.print("다시 하시겠습니까?(y/n)>>");
+			String answer = scanner.next();
+			if ("y".equals(answer) == false) { // "y"가 아니면
+				break;
+			}
+		}
+		
+		// 자원 정리
+		scanner.close();
 	}
 }
